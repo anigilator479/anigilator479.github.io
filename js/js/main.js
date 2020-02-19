7705a8113ff68b18150a4622ce5875b3592967f8
anigilator479 <56478151+anigilator479@users.noreply.github.com>
2020-02-19 19:30:20 +0200

    Add files via upload

---
+++ js/js/main.js
@@ -0,0 +1,80 @@
+// let str = "привет";
+// let num = 123;
+// let flag = true
+// let txt = "true"
+
+// console.log(typeof(str))
+// console.log(typeof(123))
+// console.log(typeof(true))
+// console.log(typeof("true"))
+
+
+// let number=123,
+//      object={},
+//      array=[],
+//      text="text",
+//     flag=true;
+
+// console num=456,
+//         obj={},
+//         a=[],
+//         txt="txt",
+//         T=true;
+
+
+// let myAge=13
+// let vanyaAge=13
+// alert(myAge+vanyaAge)
+
+
+// let a=108,
+//     b=223,
+//     c=2,
+//     d=5;
+//     console.log(a+b-c*d)
+
+
+
+// let  a1=5 + 3,
+//      a2=5 - 3,
+//      a3=5 * 3,
+//      a4=5/3;
+// console.log(a1)
+// console.log(a2)
+// console.log(a3)
+// console.log(a4)
+
+
+
+//  let a6=5 % 3,
+//      a7=3 % 5,
+//      a8=5 + '3',
+//      a9='5' - 3,
+//      a10=75 + 'кг';
+     
+//      alert(a6)
+//      alert(a7)
+//      alert(a8)
+//      alert(a9)
+//      alert(a10)
+
+
+// let height=23,
+//     width=10,
+//     s=height*width;
+//     alert(s)
+
+
+// let heightC=10,
+//     dC=4;
+//     dC=dC/2,
+//     v=3.14 * dC**2 * heightC;
+//     console.log(v)
+
+
+
+let n=3,
+    m=4,
+    k;
+    k=Math.sqrt(n**2+m**2)
+    console.log(k)
