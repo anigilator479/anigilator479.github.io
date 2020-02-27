44df99f5c68ccd8cd8d8bca6fa16c82708588d48
anigilator479 <56478151+anigilator479@users.noreply.github.com>
2020-02-27 20:27:15 +0200

    Add files via upload

---
+++ project 1/js/main.js
@@ -0,0 +1,51 @@
+1
+// let k1="5px",
+//     k2="12qwe74",
+//     k3="12.46qwe79",
+//     k4="qweqwe";
+
+// console.log(Number.parseInt(k1)); 
+// console.log(Number.parseInt(k2));
+// console.log(Number.parseInt(k3));
+// console.log(Number.parseInt(k4));
+
+
+
+2
+// let k1=5px,
+//     k2=12qwe74,
+//     k3=12.46qwe79,
+//     k4=qweqwe;
+
+// console.log(Number.parseFloat(k1)); 
+// console.log(Number.parseFloat(k2));
+// console.log(Number.parseFloat(k3)); 
+// console.log(Number.parseFloat(k4));
+3
+// let random ;
+//     console.log(Math.random(random) * ((10 - 1) + 1)**2);
+4
+// let a1='My ',
+//     a2='name',
+//     a3=' is',
+//     FullName=a1+a2+a3;
+//     FullName=FullName+' Viktor';
+// console.log(FullName);
+5
+// result = 5 + 5+'5';
+// console.log(result); 
+// console.log(typeof result); 
+
+6
+// const num=20;
+// const result = num > 10 && num < 30;
+// console.log(result)
+7
+// const num=40;
+// const result = num < 10 || num > 30;
+// console.log(result)
+8
+// console.log(!true); 
+// console.log(!false);
+// console.log(!1); 
+// console.log(!0);
