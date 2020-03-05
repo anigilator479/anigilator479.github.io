e6ef90b53bf1a71bf9170f95933a209fc440e306
anigilator479 <56478151+anigilator479@users.noreply.github.com>
2020-03-05 17:59:55 +0200

    Update main.js

--- js/js/main.js
+++ js/js/main.js
@@ -3,10 +3,10 @@
 //     flag = true,
 //     txt = "true";
 
-// console.log(typeof(str))
-// console.log(typeof(123))
-// console.log(typeof(true))
-// console.log(typeof("true"))
+// console.log(typeof(str));
+// console.log(typeof(123));
+// console.log(typeof(true));
+// console.log(typeof("true"));
 
 
 // let number=123,
@@ -15,7 +15,7 @@
 //      text="text",
 //      flag=true;
 
-// let const num=456,
+// const num=456,
 //         obj={},
 //         a=[],
 //         txt="txt",
@@ -23,14 +23,14 @@
 
 
 // let myAge=13,
-//     vanyaAge=13,
+//     vanyaAge=13;
 // alert(myAge+vanyaAge);
 
 
 // let a=108,
 //     b=223,
 //     c=2,
-//     d=5,
+//     d=5;
 //     console.log(a+b-c*d);
 
 
@@ -38,10 +38,10 @@
 // let  a1=5 + 3,
 //      a2=5 - 3,
 //      a3=5 * 3,
-//      a4=5/3,
-// console.log(a1),
-// console.log(a2),
-// console.log(a3),
+//      a4=5/3;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
 // console.log(a4);
 
 
@@ -50,25 +50,25 @@
 //      a7=3 % 5,
 //      a8=5 + '3',
 //      a9='5' - 3,
-//      a10=75 + 'кг',
+//      a10=75 + 'кг';
      
-//      alert(a6),
-//      alert(a7),
-//      alert(a8),
-//      alert(a9),
+//      alert(a6);
+//      alert(a7);
+//      alert(a8);
+//      alert(a9);
 //      alert(a10);
 
 
 // let height=23,
 //     width=10,
-//     s=height*width,
+//     s=height*width;
 //     alert(s);
 
 
 // let heightC=10,
 //     dC=4,
 //     dC=dC/2,
-//     v=3.14 * dC**2 * heightC,
+//     v=3.14 * dC**2 * heightC;
 //     console.log(v);
 
 
@@ -76,5 +76,5 @@
 let n=3,
     m=4,
     k;
-    k=Math.sqrt(Math.pow(n,2)+Math.pow (m,2))
-    console.log(k)
+    k=Math.sqrt(Math.pow(n,2)+Math.pow (m,2));
+    console.log(k);
