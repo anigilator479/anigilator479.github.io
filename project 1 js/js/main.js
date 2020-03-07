aba45e39b44e53a5247c07d3478fee7a739d5b40
anigilator479 <56478151+anigilator479@users.noreply.github.com>
2020-03-07 21:35:43 +0200

    Add files via upload

---
+++ project 1 js/js/main.js
@@ -0,0 +1,76 @@
+// 1
+
+// a="1";
+// if (a>2){
+//     console.log(a)
+// }
+// else{
+//     console.log("сьогодні перше число")
+// }
+
+// 2
+// let a=1;
+// if (a=2a){
+//     console.log(a);
+// }
+// else{
+//     console.log("upppss")
+// }
+
+// 3
+// if (5%2==0){
+//    console.log(5%2+"грн")
+// }else{
+//     console.log(5%2+"euro")
+// }
+
+
+
+// 4
+// let max="55";
+//  if (max<=16){
+//     alert("діти ")
+//  }
+// else if (max<=60){
+//     alert("дорослі")
+// } else if (max>=100){
+//     alert("пенсіонери")
+// }
+
+
+// 5
+//  let a,
+//      b;
+//     a=Math.round(Math.random());
+//     b=Math.round(Math.random());
+//     if (a==b){
+//     console.log("ФАРТ")
+// }
+
+// 6 
+// let name="Святослав",
+//     surname="Цицюра",
+//     a=name.length,
+//     b=surname.length;
+//     if (a>4&&b>5){
+//         alert(a+b);
+//     }else{
+//         alert()
+//     }
+
+7
+let a;
+a=Math.floor(Math.random() * 6);
+if (a===0){
+    console.log("0")
+}if (a===1){
+    console.log("1")
+}else if (a===2){
+    console.log("2")
+}else if (a===3){
+    console.log("3")
+}else if (a===4){
+    console.log("4")
+}else if (a===5){
+    console.log("5")
+}
