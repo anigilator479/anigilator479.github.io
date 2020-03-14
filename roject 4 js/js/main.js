1fe7cd883cedb6061f59f86b74ab242b388b3aa3
anigilator479 <56478151+anigilator479@users.noreply.github.com>
2020-03-14 14:03:51 +0200

    Add files via upload

---
+++ roject 4 js/js/main.js
@@ -0,0 +1,67 @@
+// 1
+// for (let i = 0; i < 11; i += 1) {
+//     console.log(i);
+//   }
+
+// 2
+// let a;
+// for (let i = 0; i < 11; i+=1) {
+//         if (i%2==0){
+//             console.log("Число парнe")
+//         }
+//         else{
+//             console.log("Число не парнe")
+//         }
+// }
+// 3
+// let num = 20;
+// const maxNum = 28;
+
+// while (num < maxNum) {
+//   console.log(num);
+//   num += 1;
+// }
+// 4
+// let name = '';
+
+// do {
+//   hero = prompt('');
+// } while (hero.length < 6);
+
+// console.log(hero);
+// 5
+// let a=[5,3,6,2,4],
+//     result=0;
+// for (let i = 0; i < a.length; i+=1){
+//     console.log(a[i]);
+//     result+=a[i]
+// }
+// console.log(result)
+
+
+
+// 6
+// let a=[-2,5,10,-3,8,-5,2],
+//     result=0;
+//     for (let i = 0; i < a.length; i+=1){
+//     if (a[i]>0){
+//         result+=a[i];
+//     }
+// }
+// console.log(result);
+
+// 7
+// let a = [1,2,3,4,5,6],len = a.length;
+//         result="";
+// for (let i = 0; i < len; i++){ 
+//     result+=a[i] + "-";
+//   alert(result)
+// }
+
+8
+let n=1000,
+    i=0;
+    for (i; n>50 ; i++){
+        n=n/2
+    }
+    alert(i)
