e99f55549422aa9fe1f37d751c2c501d9d51b25e
anigilator479 <56478151+anigilator479@users.noreply.github.com>
2020-03-20 20:42:08 +0200

    Add files via upload

---
+++ project 5 js/js/main.js
@@ -0,0 +1,38 @@
+// 1  
+    // let Arr1=[1, 5, "4", "hello"],
+    //     Arr2=[true,2, {}, ["a"], 222]
+    //     console.log(Arr1[1])
+    //     console.log(Arr2[1])
+    //     console.log(Arr1[1]+Arr2[1])
+// 2
+// let Arr1=[1, 5, "4", "hello"];
+//     Arr1.push(22);
+//     console.log(Arr1);
+// 3
+// let Arr2=[true,2, {}, ["a"], 222],
+//     Arr1;
+// for (Arr1 of Arr2 ){
+//   console.log(typeOf Arr1);
+// }
+// 4
+// let result = 0;
+// const matrix =  [
+//     [10,11,12],
+//     [13,14,15],
+//     [16,17,18],
+// ];
+// for (let i = 0; i < matrix.length; i += 1) {
+//     console.log (matrix[i]);
+  
+//     for (let j = 0; j < matrix[i].length; j += 1) {
+//       console.log(matrix[i][j]);
+//       result += matrix[i][j];
+//     }
+//   }
+// 5
+// const message = 'Welcome to Ukraine!';
+// a=message.split([]);
+// console.log(a)
+// console.log(message.indexOf('l'));
+// console.log(a.join(''));
+// 6
