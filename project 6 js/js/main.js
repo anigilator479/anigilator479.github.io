cae39b6b08c8f7f9030440cf5657761135795363
anigilator479 <56478151+anigilator479@users.noreply.github.com>
2020-03-26 18:14:22 +0200

    Add files via upload

---
+++ project 6 js/js/main.js
@@ -0,0 +1,37 @@
+// 1
+// function hello1(){
+//     return alert("Привіт JavaScript")
+// }
+// hello1();
+
+// 2
+// let name="Василь";
+// function hello2(){
+//  return alert("Привіт," + name)
+// }
+// hello2();
+// 3
+// function mul (n, m){
+//     return  alert(n + m),
+//             alert(n - m),
+//             alert(n * m);
+// }
+// mul(2,3);
+// 4
+// function repeat (str,n){
+//     for(let i = 0; i < n; i++){
+//         console.log(str);
+//     }
+// }
+// repeat("",2)
+// 5
+// function avg (a,b,c){
+//     for(i=0;i<avg.length;i++){
+//         z=a+b+c
+//     }
+// midNum=z/avg.length
+// console.log(midNum)
+// }
+// avg(1,2,3);
+
+// 6
